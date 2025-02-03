64b3569b69fa80c36e5956f5b82d661b8bad7c85
zaurqurbanov <qurbanovzaur078@gmail.com>
2025-02-04 02:05:30 +0400

    zaur/feat:change chat style

--- src/component/AiChat.jsx
+++ src/component/AiChat.jsx
@@ -1,55 +1,52 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { Atom } from "react-loading-indicators";
 const AiChat = () => {
   const [ask, setAsk] = useState("");
   const [loading, setLoading] = useState(false);
   const [sessionId, setSessionId] = useState("");
   const [chatArea, setChatArea] = useState();
-  const baseUrl = "https://azelawai.onrender.com/"
+  const baseUrl = "https://azelawai.onrender.com/";
   const handleAskClick = async () => {
     const data = {
       question: ask,
       session_id: sessionId,
     };
     try {
-        if(ask === "" || !ask.trim()){
-            
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Boş sual olmaz!",
-              });
-            return
-        }
-        setLoading(true);
+      if (ask === "" || !ask.trim()) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Boş sual olmaz!",
+        });
+        return;
+      }
+      setLoading(true);
       const response = await axios.post(`${baseUrl}ask`, data);
       if (response.status === 200) {
         const data = response?.data;
 
         setLoading(false);
-        setAsk("")
+        setAsk("");
         setSessionId(data.sessionId);
       }
     } catch (error) {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Boş sual olmaz!",
-          });
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Boş sual olmaz!",
+      });
     }
   };
-  
+
   useEffect(() => {
     const fetchApi = async () => {
       try {
-        const response = await axios.get(
-          `${baseUrl}history/${sessionId}`
-        );
+        const response = await axios.get(`${baseUrl}history/${sessionId}`);
         if (response.status === 200) {
           const data = response?.data;
-          
+
           setChatArea(data);
         }
       } catch (error) {}
@@ -57,32 +54,58 @@ const AiChat = () => {
 
     fetchApi();
   }, [loading]);
-  
-  
+
   return (
     <div className="w-full ">
       <div className="min-h-[60vh]">
         {chatArea?.data.map((data) => {
           return (
             <div className="p-5 " key={data._id}>
-              <div className="flex w-full items-stretch gap-2">
-                <p className="font-semibold ">YOU:</p>
-                <p className="p-5 bg-amber-100 w-full rounded-2xl">
-                  {data?.userMessage}
-                </p>
+              <div className="flex items-start w-full gap-2.5">
+                <div className="flex flex-col w-full  leading-1.5 shadow p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                  <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                    <span className="text-sm font-semibold text-gray-900 dark:text-white">
+                      YOU
+                    </span>
+                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                      11:46
+                    </span>
+                  </div>
+                  <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
+                    {data?.userMessage}
+                  </p>
+                </div>
               </div>
 
-              <div className="p-5 flex  gap-2 flex-row-reverse flex-wrap">
-                <p className=" font-semibold text-end">LAW_AI</p>
-                <p className=" bg-blue-50 p-5 rounded-4xl shadow-2xl">
-                  {data.botMessage}
-                </p>
+              <div className="flex items-start w-full gap-2.5 mt-9">
+                <div className="flex flex-col w-full  leading-1.5 shadow-xl p-4 border-gray-500 bg-gray-200 rounded-s-2xl rounded-ee-xl dark:bg-gray-700">
+                  <div className="flex items-center space-x-2 justify-end">
+                  <span className="text-sm font-semibold text-gray-900 dark:text-white">
+                    LAW_AI
+                    </span>
+                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                      11:46
+                    </span>
+                  </div>
+                  <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
+                    {data?.botMessage}
+                  </p>
+                    
+                </div>
               </div>
+
+           
+
+             
             </div>
           );
         })}
       </div>
-      {loading && <div className="flex items-center justify-center"><Atom color="#32cd32" size="medium" text="" textColor="" /></div>}
+      {loading && (
+        <div className="flex items-center justify-center">
+          <Atom color="#32cd32" size="medium" text="" textColor="" />
+        </div>
+      )}
       <div className=" w-full p-8">
         <textarea
           type="text"
@@ -90,10 +113,12 @@ const AiChat = () => {
           onChange={(e) => {
             setAsk(e.target.value);
           }}
-          className="w-full  h-36 border rounded-2xl p-2 shadow-2xl  "
+          rows={5}
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Write your thoughts here... "
         />
         <button
-          className="cursor-pointer p-2 mt-2 w-full bg-blue-300"
+          className="cursor-pointer p-2 mt-2 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => handleAskClick()}
         >
           Ask
