3519a94f551e77c09aede22302e6e47cf797fcaf
zaurqurbanov <qurbanovzaur078@gmail.com>
2025-02-04 01:42:39 +0400

    zaur/change ui

--- src/component/AiChat.jsx
+++ src/component/AiChat.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Swal from 'sweetalert2'
+import { Atom } from "react-loading-indicators";
 const AiChat = () => {
   const [ask, setAsk] = useState("");
   const [loading, setLoading] = useState(false);
@@ -59,20 +60,20 @@ const AiChat = () => {
   
   
   return (
-    <div className="w-full font-mono">
+    <div className="w-full ">
       <div className="min-h-[60vh]">
         {chatArea?.data.map((data) => {
           return (
-            <div className="p-5">
-              <div className="">
-                <p className="font-semibold">YOU:</p>
-                <p className="p-5 bg-amber-100 rounded-3xl">
+            <div className="p-5 " key={data._id}>
+              <div className="flex w-full items-stretch gap-2">
+                <p className="font-semibold ">YOU:</p>
+                <p className="p-5 bg-amber-100 w-full rounded-2xl">
                   {data?.userMessage}
                 </p>
               </div>
 
-              <div className="p-5">
-                <p className=" font-semibold text-end">bot:</p>
+              <div className="p-5 flex  gap-2 flex-row-reverse flex-wrap">
+                <p className=" font-semibold text-end">LAW_AI</p>
                 <p className=" bg-blue-50 p-5 rounded-4xl shadow-2xl">
                   {data.botMessage}
                 </p>
@@ -81,7 +82,7 @@ const AiChat = () => {
           );
         })}
       </div>
-      {loading && <h1>LOADINGGGGGGG...</h1>}
+      {loading && <div className="flex items-center justify-center"><Atom color="#32cd32" size="medium" text="" textColor="" /></div>}
       <div className=" w-full p-8">
         <textarea
           type="text"
@@ -89,7 +90,7 @@ const AiChat = () => {
           onChange={(e) => {
             setAsk(e.target.value);
           }}
-          className="w-full  h-36 border shadow-2xl  "
+          className="w-full  h-36 border rounded-2xl p-2 shadow-2xl  "
         />
         <button
           className="cursor-pointer p-2 mt-2 w-full bg-blue-300"
